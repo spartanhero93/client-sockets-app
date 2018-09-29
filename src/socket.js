7cc47063a673a42947607cdf00bc0dadc699f8b7
Luis Lopez <name.luislopez@gmail.com>
2018-09-29 19:45:37 -0400

    setup connection to ec2 instance

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client'
 
-const socket = io('https://sockets-io-server.herokuapp.com/')
+const socket = io('ec2-52-87-178-69.compute-1.amazonaws.com:3000')
 
 export default socket
